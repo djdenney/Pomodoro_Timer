39a215b1b34b97d69a87b12241d6ddeadfa33d80
djdenney <daniel@denneythe.dev>
2021-04-29 13:28:49 -0700

    Updates

---
+++ precache-manifest.90fa426bb8f86d3da0c0987e899dac9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f8ab5a262bd67b3923370fbc919918a",
+    "url": "/Pomodoro_Timer/index.html"
+  },
+  {
+    "revision": "760453c66e8f8d3a3add",
+    "url": "/Pomodoro_Timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "223b709c9b9c0b308480",
+    "url": "/Pomodoro_Timer/static/js/2.dcb25545.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Pomodoro_Timer/static/js/2.dcb25545.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "760453c66e8f8d3a3add",
+    "url": "/Pomodoro_Timer/static/js/main.49346f7a.chunk.js"
+  },
+  {
+    "revision": "a647b2b62fdb8a5e7f8a",
+    "url": "/Pomodoro_Timer/static/js/runtime-main.7b75ae4c.js"
+  }
+]);
